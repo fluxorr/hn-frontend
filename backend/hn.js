ded1b47fcfd85f556cc8d78f42b532c1a9a9fd01
Rahul Chaudhari_ <rahulgc545@gmail.com>
2025-02-23 00:41:05 +0530

    basic

---
+++ backend/hn.js
@@ -0,0 +1,21 @@
+async function getHNStories() {
+  const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+  const storyIds = await response.json();
+
+  const stories = await Promise.all(
+    storyIds.slice(0, 30).map(async (id) => {
+      const storyResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+      const story = await storyResponse.json();
+      return story;
+    })
+  );
+  // console.log(stories);
+  return stories;
+}
+
+getHNStories()
+
+export default getHNStories;
+
+
+
